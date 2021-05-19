a02b9b50156013b9e04e3f251bd8d7b242ecb2ca
pzxbjx <1426706793@qq.com>
2021-05-19 15:30:56 +0800

    Add mrc metric & unitest of mrc metric

---
+++ test/input_layer/model/metrics/test_metrics.py
@@ -0,0 +1,17 @@
+import unittest
+from textflint.input_layer.model.metrics.metrics import *
+
+
+class TestMetrics(unittest.TestCase):
+    def test_squad_metric(self):
+        predictions = [
+            {'prediction_text': '1976', 'id': '56e10a3be3433e1400422b22'}]
+        references = [{'answers': {'answer_start': [97], 'text': ['1976']},
+                       'id': '56e10a3be3433e1400422b22'}]
+        score = squad_metric(predictions, references)
+        self.assertEqual(100.0, score['exact_match'])
+        self.assertEqual(100.0, score['f1'])
+
+
+if __name__ == '__main__':
+    unittest.main()
