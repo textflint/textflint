b4b8183edc28042d54ebdd790d50585cadae47bf
pzxbjx <1426706793@qq.com>
2021-05-19 15:56:48 +0800

    fix mrc metric & mrc-metric unitest

--- test/input_layer/model/metrics/test_metrics.py
+++ test/input_layer/model/metrics/test_metrics.py
@@ -8,6 +8,7 @@ class TestMetrics(unittest.TestCase):
             {'prediction_text': '1976', 'id': '56e10a3be3433e1400422b22'}]
         references = [{'answers': {'answer_start': [97], 'text': ['1976']},
                        'id': '56e10a3be3433e1400422b22'}]
+        squad_metric = SQuADMetric()
         score = squad_metric(predictions, references)
         self.assertEqual(100.0, score['exact_match'])
         self.assertEqual(100.0, score['f1'])
--- textflint/input_layer/model/metrics/metrics.py
+++ textflint/input_layer/model/metrics/metrics.py
@@ -6,22 +6,26 @@ from sklearn.metrics import accuracy_score, \
     f1_score
 
 
-def squad_metric(predictions, references):
-    r"""
-    Computes SQuAD scores (F1 and EM).
-    :param list predictions:
-    List of question-answers dictionaries with the following key-values:
-        - 'id': id of the question-answer pair as given in the references
-        - 'prediction_text': the text of the answer
-    :param list references:
-    List of question-answers dictionaries with the following key-values:
-        - 'id': id of the question-answer pair (see above),
-        - 'answers': a Dict in the SQuAD dataset format
-            {
-                'text': list of possible texts for the answer
-                'answer_start': list of start positions for the answer
-            }
-    :return: dict obj to save metrics result
-    """
-    metric = datasets.load_metric("squad")
-    return metric.compute(predictions=predictions, references=references)
+class SQuADMetric(object):
+    def __init__(self):
+        self.metric = datasets.load_metric("squad")
+
+    def __call__(self, predictions, references):
+        r"""
+        Computes SQuAD scores (F1 and EM).
+        :param list predictions:
+        List of question-answers dictionaries with the following key-values:
+            - 'id': id of the question-answer pair as given in the references
+            - 'prediction_text': the text of the answer
+        :param list references:
+        List of question-answers dictionaries with the following key-values:
+            - 'id': id of the question-answer pair (see above),
+            - 'answers': a Dict in the SQuAD dataset format
+                {
+                    'text': list of possible texts for the answer
+                    'answer_start': list of start positions for the answer
+                }
+        :return: dict obj to save metrics result
+        """
+        return self.metric.compute(
+            predictions=predictions, references=references)
